4b2f20fd0cf423d38f9d3fa95587e870931bd15a
Anton <darkneverdie@mail.ru>
2020-10-25 22:22:38 +0700

    Отправить сообщение другу

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import secret
 import time
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.get('https://vk.com')
@@ -14,5 +15,13 @@ button.click()
 driver.implicitly_wait(10)
 time.sleep(5)
 driver.get('https://vk.com/' + secret.IDFr)
-
+driver.implicitly_wait(10)
+#Найти кнопку Написать сообщение и щелкнуть
+button = driver.find_element(By.XPATH, '//button[text()="Написать сообщение"]')
+button.click()
+#Отправить сообщение
+sb = driver.find_element_by_css_selector('#mail_box_editable')
+sb.send_keys('Привет')
+button = driver.find_element_by_css_selector('#mail_box_send')
+button.click()
 
